715c407615f37f4aa30915d7784238b750663bd0
Mark Leighton Fisher <markleightonfisher@gmail.com>
2014-09-10 15:54:52 -0400

    v0.3 - Added GradeLevel
    1) Bumped version to v0.3.
    2) Added GradeLevel for Kindergarten, 1st, etc.
    3) Fixed Class to use GradeLevel instead of Grade (a class does not
       have a numerical grade by itself).

--- School/Schema/Result/GradeLevel.pm
+++ School/Schema/Result/GradeLevel.pm
@@ -1,4 +1,4 @@
-package School::Schema::Result::Class;
+package School::Schema::Result::GradeLevel;
 use parent qw( DBIx::Class::Core );
 
 use utf8;
@@ -6,7 +6,7 @@ use autodie;
 use warnings;
 use strict;
 
-__PACKAGE__->table('class');
+__PACKAGE__->table('grade_level');
 __PACKAGE__->add_columns(
     id => {
         data_type         => 'integer',
@@ -14,20 +14,30 @@ __PACKAGE__->add_columns(
         is_numeric        => 1,
         is_auto_increment => 1,
     },
-    grade_level_id => {
-        data_type         => 'integer',
-        is_nullable       => 0,
-        is_numeric        => 1,
-        is_foreign_key    => 1,
-    },
     name => {
         data_type         => 'text',
         is_numeric        => 0,
         is_nullable       => 0,
     },
+    order => {
+        data_type         => 'integer',
+        is_numeric        => 1,
+        is_nullable       => 0,
+    },
 );
 __PACKAGE__->set_primary_key('id');
-__PACKAGE__->has_many(  students    => 'School::Schema::Result::Student',    'id');
-__PACKAGE__->belongs_to(grade_level => 'School::Schema::Result::GradeLevel', 'id');
+__PACKAGE__->has_many(classes => 'School::Schema::Result::Class', 'id');
 
 1;
+
+=pod
+
+=head1 NOTES
+
+=over 4
+
+=item * 'order' is so you can display 'Preschool, Kindergarten, 1st ...'
+
+=back
+
+=cut
